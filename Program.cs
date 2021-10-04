a85b7d1c7d8349600489d09dd8d6317550c27dd8
ehinners <ehinners@my.wctc.edu>
2021-10-04 08:19:47 -0500

    Init Commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using NLog.Web;
+using System.Collections.Generic;
+
+namespace MediaLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("");
+
+            //////////////////////////////
+            //      NLOG Instantiation  //
+            //////////////////////////////
+
+            string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+
+            // create instance of Logger
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog(path).GetCurrentClassLogger();
+
+            logger.Info("NLOG Loaded");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
