8919d26ca896811a7011d3b0e6f89d4e4e29ec01
ehinners <ehinners@my.wctc.edu>
2021-10-04 08:39:41 -0500

    Added Album class

--- Media.cs
+++ Media.cs
@@ -37,5 +37,15 @@ namespace MediaLibrary
     }
 
 
-    
+        // Album class is derived from Media class
+    public class Album : Media
+    {
+        public string artist { get; set; }
+        public string recordLabel { get; set; }
+
+        public override string Display()
+        {
+            return $"Id: {mediaId}\nTitle: {title}\nArtist: {artist}\nLabel: {recordLabel}\nGenres: {string.Join(", ", genres)}\n";
+        }
+    }
 }
